93c198aa69ceaabb48c2c264e6a800f7976dddd5
KobiManuel <Emmakobi91@gmail.com>
2023-05-03 10:27:41 +0100

    feat(User Interface): populated and styled Hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.svg";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className=" w-full !flex flex-row justify-between items-center mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button className="black_btn" type="button">
+          Twitter
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize articles with <br className=" max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with SynthSpeak, an open source article summarizer
+        that transforms lengthy articlees into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
